2d6cbe32a411fb7d9e4d7e777b56860759171884
Karl Kedrovsky <karl@kedrovsky.com>
2016-07-23 11:43:25 -0500

    Cleaned up single file components.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var webpack = require('webpack-stream');
 
 var sassPaths = [
   'node_modules/foundation-sites/scss'
@@ -22,11 +23,43 @@ gulp.task('js', function() {
     .pipe(gulp.dest('web/js'));
 });
 
-gulp.task('default', ['sass', 'js'], function() {
+gulp.task('webpack', function() {
+  return gulp.src('js_sfc/sfc.js')
+    .pipe(webpack({
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel'
+          },
+          {
+            test: /\.vue$/,
+            loader: 'vue'
+          }
+        ]
+      },
+      babel: {
+        presets: ['es2015'],
+        plugins: ['transform-runtime']
+      },
+      resolve: {
+        modulesDirectories: ['node_modules']
+      },
+      output: {
+        filename: 'sfc.js'
+      }
+    }))
+    .pipe(gulp.dest('web/js/'));
+});
+
+gulp.task('default', ['sass', 'js', 'webpack'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
   gulp.watch('node_modules/foundation-sites/dist/*.js', ['js']);
   gulp.watch('node_modules/foundation-sites/vendor/jquery/dist/*.js', ['js']);
   gulp.watch('node_modules/vue/dist/*.js', ['js']);
   gulp.watch('node_modules/vue-router/dist/*.js', ['js']);
   gulp.watch('js/*.js', ['js']);
+  gulp.watch('js_sfc/**/*.js', ['webpack']);
+  gulp.watch('js_sfc/**/*.vue', ['webpack']);
 });
---
+++ js_sfc/components/demo.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Demo Component</h1>
+  <p class="red">{{ message }}</p>
+</template>
+<script>
+ export default {
+   name: "Demo",
+   data() {
+     return {
+       message: "Woohoo!"
+     }
+   }
+ }
+</script>
+<style>
+  .red {
+    color: #ee2222
+  };
+</style>
